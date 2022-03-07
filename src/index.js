2668d24e253ba83c40028e30df3cd28398543988
Mi Tam Nguyen <mitamnguyen@Mis-MacBook-Air.local>
2022-03-07 16:17:13 -0600

    DOM-I complete

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const siteContent = { // DO NOT CHANGE THIS OBJECT
-  "nav": {
+  "nav": { 
     "nav-item-1": "Services",
     "nav-item-2": "Product",
     "nav-item-3": "Vision",
@@ -40,3 +40,40 @@ const siteContent = { // DO NOT CHANGE THIS OBJECT
 };
 
 console.log('project wired!')
+
+const navLinks = document.querySelectorAll('header nav a')
+const navLinksTexts = Object.values(siteContent.nav)
+navLinks.forEach((link, idx) => {
+  link.textContent = navLinksTexts[idx]
+  link.classList.add('italic')
+})
+document.querySelector('.cta .cta-text h1').textContent = siteContent.cta.h1
+document.querySelector('.cta .cta-text button').textContent = siteContent.cta.button
+const footerLink = document.querySelector('footer a');
+footerLink.textContent = siteContent.footer.copyright;
+footerLink.classList.add('bold');
+const contact = document.querySelector('section.contact');
+contact.querySelector('h4').textContent = siteContent.contact['contact-h4']
+contact.querySelector('p:nth-of-type(1)').textContent = siteContent.contact['address']
+contact.querySelector('p:nth-of-type(2)').textContent = siteContent.contact['phone']
+contact.querySelector('p:nth-of-type(3)').textContent = siteContent.contact['email']
+const botContent = document.querySelector('.bottom-content');
+const h4sBottom = botContent.querySelectorAll('h4');
+h4sBottom[0].textContent = siteContent["main-content"]["services-h4"];
+h4sBottom[1].textContent = siteContent["main-content"]["product-h4"];
+h4sBottom[2].textContent = siteContent["main-content"]["vision-h4"];
+const pBottom = botContent.querySelectorAll('p');
+pBottom[0].textContent = siteContent["main-content"]["services-content"];
+pBottom[1].textContent = siteContent["main-content"]["product-content"];
+pBottom[2].textContent = siteContent["main-content"]["vision-content"];
+const topContent = document.querySelector('.top-content');
+topContent.children[0].children[0].textContent = siteContent["main-content"]["features-h4"]
+topContent.children[0].children[1].textContent = siteContent["main-content"]["features-content"]
+topContent.children[1].children[0].textContent = siteContent["main-content"]["about-h4"]
+topContent.children[1].children[1].textContent = siteContent["main-content"]["about-content"]
+const logoImg = document.querySelector("#logo-img");
+logoImg.src = siteContent.images['logo-img']
+const ctaImg = document.querySelector('#cta-img');
+ctaImg.src = siteContent.images['cta-img'];
+const midImg = document.querySelector('#middle-img');
+midImg.src = siteContent.images['accent-img'];
